3d9f1a1fce923b96c5e84563e17db7de777542d3
Kamil Jarząb <jarzbkamil@gmail.com>
2020-12-29 15:03:08 +0100

    Added shooting intervals

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,9 +13,11 @@ public class PlayerController : MonoBehaviour
 
     public GameObject horizontalProjectile;
     public GameObject verticalProjectile;
-
+    public int shotIntervalsInSeconds;
+    private bool _shootReady = true;
+    
     public float jumpHeight;
-    private bool jumpReady = true;
+    private bool _jumpReady = true;
 
     private Rigidbody playerRigidBody;
     
@@ -41,15 +45,12 @@ public class PlayerController : MonoBehaviour
         if (ShouldPlayerJump())
         {
             playerRigidBody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-            jumpReady = false;
-            Debug.Log("JUMNP ");
-
+            _jumpReady = false;
         }
     }
 
     private void HandleMovement()
     {
-        //float currentPlayerSpeed = playerRigidBody.velocity.sqrMagnitude;
         if (ShouldPlayerAccelerate()) 
         {
             playerSpeed += speedInterval;
@@ -57,8 +58,12 @@ public class PlayerController : MonoBehaviour
         {
             playerSpeed -= speedInterval;
         }
-        //Debug.Log("Speed " + playerSpeed );
 
+        SetPlayerSpeed();
+    }
+
+    private void SetPlayerSpeed()
+    {
         playerRigidBody.velocity = new Vector3(playerSpeed, 0, 0);
 
     }
@@ -67,9 +72,35 @@ public class PlayerController : MonoBehaviour
     {
         if (ShouldPlayerShoot())
         {
-            Instantiate(horizontalProjectile, transform.position, horizontalProjectile.transform.rotation);
-            Instantiate(verticalProjectile, transform.position, verticalProjectile.transform.rotation);
+            CreateVerticalProjectile();
+            CreateHorizontalProjectile();
+            ReloadShoot();
         }
+    }
+
+    private void ReloadShoot()
+    {
+        DisableShoot();
+        Task.Delay(shotIntervalsInSeconds * 1000).ContinueWith(t=> EnableShoot());
+    }
+
+    private void DisableShoot()
+    {
+        _shootReady = false;
+    }
+    private void EnableShoot()
+    {
+        _shootReady = true;
+    }
+
+    private void CreateVerticalProjectile()
+    {
+        Instantiate(verticalProjectile, transform.position, verticalProjectile.transform.rotation);
+    }
+
+    private void CreateHorizontalProjectile()
+    {
+        Instantiate(horizontalProjectile, transform.position, horizontalProjectile.transform.rotation);
     }
 
     private bool ShouldPlayerAccelerate()
@@ -85,24 +116,19 @@ public class PlayerController : MonoBehaviour
 
     private bool ShouldPlayerJump()
     {
-        return (Input.GetKeyDown(KeyCode.UpArrow) && jumpReady);
+        return (Input.GetKeyDown(KeyCode.UpArrow) && _jumpReady);
     }
 
     private bool ShouldPlayerShoot(){
-        return (Input.GetKeyDown(KeyCode.Space));
+        return (Input.GetKeyDown(KeyCode.Space) && _shootReady);
     }
 
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision);
-
-        //Check for a match with the specified name on any GameObject that collides with your GameObject
-        if (CompareTag("Platform")) 
+        if (collision.gameObject.CompareTag("Platform")) 
         {
-            
-            Debug.Log("JUMP READY");
-            jumpReady = true;
+            _jumpReady = true;
         }
 
     }
