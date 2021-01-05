8b9e6f8fdd639e4dabfebcf5f85a11720fe0c481
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-05 18:33:20 +0100

    Added enemy spawning intervals

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -14,23 +14,44 @@ public class Spawn : MonoBehaviour
     private float _topBound;
     private float _bottomBound;
     private Vector3 _enemiesSpawnPosition;
+
+    public float minWaitBeforeSpawn;
+    public float maxWaitBeforeSpwan;
+    private bool _isEnemySpawning = false;
     void Start()
     {
         _enemyArea = GameObject.FindGameObjectsWithTag("EnemyArea")[0];
         _enemyAreaBoxCollider = _enemyArea.GetComponent<BoxCollider2D>();
         CalculateEnemyAreaBounds();
+        SpawnEnemy();
+    }
+
+    void Update()
+    {
+        HandleSpawningEnemy();
+    }
+    
+    private void HandleSpawningEnemy()
+    {
+        if (!_isEnemySpawning)
+        {
+            float timer = UnityEngine.Random.Range(minWaitBeforeSpawn, maxWaitBeforeSpwan);
+            Invoke("SpawnEnemy", timer);
+            _isEnemySpawning = true;
+        }  
+    }
+ 
+    void SpawnEnemy()
+    {
         int enemyQuantity = Random.Range(2, 5);
         _enemiesSpawnPosition = GenerateNewPosition();
 
         for (int i = 0; i < enemyQuantity; i++)
         {
             Instantiate(enemyPrefab, new Vector3((_leftBound - 2) - (i * 1), _enemiesSpawnPosition.y, _enemiesSpawnPosition.z), enemyPrefab.transform.rotation);
-        }
-    }
-
-    void Update()
-    {
+        }       
         
+        _isEnemySpawning = false;
     }
     
     private void CalculateEnemyAreaBounds()
