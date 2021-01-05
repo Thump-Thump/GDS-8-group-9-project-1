63183f0d8a9fc532fe38a9793ff5b81eeec3001b
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-05 18:26:46 +0100

    Added destroying out of bounds to projectiles, bomb, enemies

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -35)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.y > 10)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.x > 15)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
